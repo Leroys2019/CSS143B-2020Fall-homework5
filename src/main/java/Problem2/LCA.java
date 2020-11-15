99e032a775572dbaa00c6f2bf7367050dbd814ea
Leroy Smith <leroys2019@gmail.com>
2020-11-15 06:43:01 -0800

    Problem #2 test passes

--- src/main/java/Problem2/LCA.java
+++ src/main/java/Problem2/LCA.java
@@ -6,6 +6,23 @@ public class LCA {
     public static TreeNode<Integer> lowestCommonAncestor(TreeNode<Integer> root, TreeNode<Integer> p, TreeNode<Integer> q) {
         // homework
         // hint: pay attention to the assumptions of this problem.
-        return null;    // place holder
+        if (root == null) {
+            return null;// If empty returns a null.
+        } else {
+            if (root == p || root == q) {
+                return root;
+            }
+            TreeNode right = lowestCommonAncestor(root.right, p, q);
+            TreeNode left = lowestCommonAncestor(root.left, p, q);
+            if (right != null && left != null) {
+                return root;
+            }
+
+            if (right != null) {
+                return right;
+            } else {
+                return left;
+            }
+        }
     }
 }
